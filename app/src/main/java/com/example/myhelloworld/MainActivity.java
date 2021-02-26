6996345942394e376b19338cabfd7df9e9401892
Vasilii Ponteleev <vvponteleev@domclick.ru>
2021-02-26 21:47:22 +0300

    Added number compare.

---
+++ app/src/main/java/com/example/myhelloworld/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.myhelloworld;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compare_digits);
+
+        EditText numberOne = findViewById(R.id.number_one);
+        EditText numberTwo = findViewById(R.id.number_two);
+        Button btnCompare = findViewById(R.id.btn_compare);
+        TextView result = findViewById(R.id.result);
+
+        btnCompare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                try {
+
+                    Integer valueOne = Integer.valueOf(numberOne.getText().toString());
+                    Integer valueTwo = Integer.valueOf(numberTwo.getText().toString());
+
+                    if (valueOne.equals(valueTwo)) {
+                        result.setText("Equal");
+                    } else {
+                        result.setText("Not equal");
+                    }
+                } catch (NumberFormatException exc) {
+                    Log.d("My awesome app", "Incorret input");
+                    result.setText("Please enter a number");
+                }
+            }
+        });
+    }
+}
